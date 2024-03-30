6ee9b415390721b49ed303baf32c7cf906b887c4
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2024-03-30 17:00:52 -0300

    chore: change typescript path

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,11 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/application/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/application/components/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/application/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/application/components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-  },
+  theme: {},
   plugins: [],
 };
 export default config;
