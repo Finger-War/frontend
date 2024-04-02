1923b090529db42ac9c05f0d9449fe2897456e47
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2024-04-02 14:00:26 -0300

    test: ensure timer component renders value correctly

---
+++ tests/application/pages/home/components/timer.spec.tsx
@@ -0,0 +1,15 @@
+import '@testing-library/jest-dom';
+
+import { render } from '@testing-library/react';
+
+import { Timer } from '@/application/pages/home/components/timer';
+
+describe('Timer component', () => {
+  it('Should renders value correctly', () => {
+    const any_value = 0;
+
+    const { getByText } = render(<Timer initialTime={any_value} />);
+
+    expect(getByText(`Time: ${any_value}`)).toBeInTheDocument();
+  });
+});
