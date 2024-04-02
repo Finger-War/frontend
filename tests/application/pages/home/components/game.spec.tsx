67aae4e3b2b7085cc0477c7b151c98adfd746321
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2024-04-02 14:19:23 -0300

    test: ensure game component trigger correct word

--- tests/application/pages/home/components/game.spec.tsx
+++ tests/application/pages/home/components/game.spec.tsx
@@ -2,13 +2,13 @@ import '@testing-library/jest-dom';
 
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
 
 import { Game } from '@/application/pages/home/components/game';
 
 describe('Game Component', () => {
   test('Should render correctly', () => {
-    const words = ['apple', 'banana', 'orange'];
+    const words = ['first', 'second', 'third'];
     const { getByPlaceholderText, getByText } = render(<Game words={words} />);
 
     words.forEach((word) => {
@@ -16,5 +16,22 @@ describe('Game Component', () => {
     });
 
     expect(getByPlaceholderText('Type here.....')).toBeInTheDocument();
+  });
+
+  test('Should trigger correct word', async () => {
+    const words = ['first', 'second', 'third'];
+    const onInputCorrectWordMock = jest.fn();
+    const { getByPlaceholderText } = render(
+      <Game words={words} onInputCorrectWord={onInputCorrectWordMock} />,
+    );
+
+    const inputElement = getByPlaceholderText('Type here.....');
+
+    fireEvent.change(inputElement, { target: { value: 'first ' } });
+    fireEvent.keyPress(inputElement, { key: 'Enter', code: 13, charCode: 13 });
+
+    await waitFor(() => {
+      expect(onInputCorrectWordMock).toHaveBeenCalledWith('first');
+    });
   });
 });
