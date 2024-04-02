29b0dfc251a91001c9eee9b74c9c4f0bd4cba36d
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2024-04-02 13:26:16 -0300

    feat: ensure home page renders correctly initials elements

--- src/application/pages/home/index.tsx
+++ src/application/pages/home/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import { Footer } from '@/application/components/layout/footer';
+import { Header } from '@/application/components/layout/header';
+
 export const Home = (): React.JSX.Element => {
   return (
-    <div>
-      <h1>Hello, Fingerwar!</h1>
+    <div className="flex flex-col min-h-screen justify-between">
+      <Header />
+      <Footer />
     </div>
   );
 };
