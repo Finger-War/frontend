0bb7a7507c599014364b411970dd48a76780e8e3
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2024-04-02 18:20:30 -0300

    test: ensure home page renders timer and game after start playing

--- tests/application/pages/home/index.spec.tsx
+++ tests/application/pages/home/index.spec.tsx
@@ -2,7 +2,7 @@ import '@testing-library/jest-dom';
 
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import { Home } from '@/application/pages/home';
 
@@ -12,5 +12,22 @@ describe('Home Page', () => {
 
     expect(getByText('Start Playing')).toBeInTheDocument();
     expect(getByText('Play')).toBeInTheDocument();
+  });
+
+  test('Should renders timer and game after start playing', async () => {
+    const { container, getByText } = render(<Home />);
+
+    const textElement = getByText('Start Playing');
+    const buttonElement = getByText('Play');
+
+    fireEvent.click(buttonElement);
+
+    const timeElement = container.querySelector('#timer');
+    const gameElement = container.querySelector('#game');
+
+    expect(buttonElement).not.toBeInTheDocument();
+    expect(textElement).not.toBeInTheDocument();
+    expect(timeElement).toBeInTheDocument();
+    expect(gameElement).toBeInTheDocument();
   });
 });
