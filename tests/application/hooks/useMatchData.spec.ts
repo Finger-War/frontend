d7dc9dcd3362d101db03126ada3c52d2ef518e3a
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2024-04-02 18:09:59 -0300

    test: ensure use match data hook returns initial state if invalid type is provided

--- tests/application/hooks/useMatchData.spec.ts
+++ tests/application/hooks/useMatchData.spec.ts
@@ -19,4 +19,16 @@ describe('UseMatchData Hook', () => {
 
     expect(result.current.matchData.wpm).toBe(1);
   });
+
+  test('Should return initial state if invalid type is provided', () => {
+    const { result } = renderHook(() => UseMatchData());
+
+    const initialMatchData = result.current.matchData;
+
+    act(() => {
+      result.current.dispatchMatchData({ type: 'unknown_action' } as any);
+    });
+
+    expect(result.current.matchData).toEqual(initialMatchData);
+  });
 });
