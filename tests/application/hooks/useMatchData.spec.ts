d4ddc3682d4e865dfdeb10682ed1c628fcbaa488
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2024-04-02 18:08:37 -0300

    test: ensure user match data hook initializes match data with default data

---
+++ tests/application/hooks/useMatchData.spec.ts
@@ -0,0 +1,34 @@
+import '@testing-library/jest-dom';
+
+import { act, renderHook } from '@testing-library/react';
+
+import { UseMatchData } from '@/application/hooks/useMatchData';
+
+describe('UseMatchData Hook', () => {
+  test('Should initialize matchData with default data', () => {
+    const { result } = renderHook(() => UseMatchData());
+    expect(result.current.matchData.wpm).toBe(0);
+  });
+
+  test('Should increment wpm correctly', () => {
+    const { result } = renderHook(() => UseMatchData());
+
+    act(() => {
+      result.current.dispatchMatchData({ type: 'increment_wpm' });
+    });
+
+    expect(result.current.matchData.wpm).toBe(1);
+  });
+
+  test('Should return initial state if invalid type is provided', () => {
+    const { result } = renderHook(() => UseMatchData());
+
+    const initialMatchData = result.current.matchData;
+
+    act(() => {
+      result.current.dispatchMatchData({ type: 'unknown_action' } as any);
+    });
+
+    expect(result.current.matchData).toEqual(initialMatchData);
+  });
+});
