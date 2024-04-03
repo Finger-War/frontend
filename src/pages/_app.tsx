7eddbe826dcbb1811f5bc217b98f45008f5856cc
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2024-04-02 23:57:21 -0300

    chore: add react query setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,18 @@ import type { AppProps } from 'next/app';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
       <Analytics />
       <SpeedInsights />
-    </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
