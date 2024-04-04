fca14dab6644bd2ebcf8b436d8d9afba623c9497
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2024-04-03 22:35:21 -0300

    chore: move global css to application

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import '../global.css';
+import '@/application/global.css';
 
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
