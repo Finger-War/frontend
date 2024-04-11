e50d7597c9b3662693d6c7b239b76f80dda3c93b
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2024-04-11 01:20:25 -0300

    test: ensure home page change word color to green if correct word is provided

--- tests/__e2e__/app/application/pages/home/index.cy.ts
+++ tests/__e2e__/app/application/pages/home/index.cy.ts
@@ -29,4 +29,17 @@ describe('Home Page', () => {
       .first()
       .should('have.class', 'text-red-600');
   });
+
+  it('Should change word color to green if correct word is provided', () => {
+    cy.visit('/');
+
+    cy.get('[data-test="play-button"]').click();
+    cy.get('[data-test="game-words"]').then(($words) => {
+      const word = $words[0].textContent;
+      cy.get('[data-test="game-input"]').type(`${word}`).type(' ');
+    });
+    cy.get('[data-test="game-words"]')
+      .first()
+      .should('have.class', 'text-green-600');
+  });
 });
