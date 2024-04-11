1d105efa987ff134537c40b85fc305037c63fcdb
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2024-04-11 02:49:02 -0300

    refactor: move visit to before each

--- tests/__e2e__/app/application/pages/home/index.cy.ts
+++ tests/__e2e__/app/application/pages/home/index.cy.ts
@@ -1,28 +1,24 @@
 describe('Home Page', () => {
-  it('Should renders home correctly', () => {
+  beforeEach(() => {
     cy.visit('/');
+  });
 
-    cy.contains('Start Playing');
-    cy.contains('Play');
+  it('Should renders home correctly', () => {
+    cy.contains('Start Playing').should('be.visible');
+    cy.contains('Play').should('be.visible');
   });
 
   it('Should render loading when words generator is fetching', () => {
-    cy.visit('/');
-
-    cy.contains('Loading');
+    cy.contains('Loading').should('be.visible');
   });
 
   it('Should renders timer and game after start playing', () => {
-    cy.visit('/');
-
     cy.get('[data-test="play-button"]').click();
     cy.get('[data-test="timer"]').should('be.visible');
     cy.get('[data-test="game"]').should('be.visible');
   });
 
   it('Should change word color to red if wrong word is provided', () => {
-    cy.visit('/');
-
     cy.get('[data-test="play-button"]').click();
     cy.get('[data-test="game-input"]').type(`any_wrong_wrong`).type(' ');
     cy.get('[data-test="game-words"]')
@@ -31,8 +27,6 @@ describe('Home Page', () => {
   });
 
   it('Should change word color to green if correct word is provided', () => {
-    cy.visit('/');
-
     cy.get('[data-test="play-button"]').click();
     cy.get('[data-test="game-words"]').then(($words) => {
       const word = $words[0].textContent;
@@ -41,5 +35,16 @@ describe('Home Page', () => {
     cy.get('[data-test="game-words"]')
       .first()
       .should('have.class', 'text-green-600');
+  });
+
+  it('Should show wpm when the match is over', () => {
+    cy.get('[data-test="play-button"]').click();
+    cy.get('[data-test="timer"]').should('exist');
+    cy.get('[data-test="game-words"]').each(($word) => {
+      const word = $word.text();
+      cy.get('[data-test="game-input"]').type(`${word} `);
+    });
+
+    cy.get('[data-test="wpm"]').should('exist');
   });
 });
